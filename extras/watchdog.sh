8e9117776113b30a256600078ee5a3e609cd98c0
Louise <louise@freedomfone.org>
2009-11-12 14:04:20 +0000

    New watchdog with different log levels

--- extras/watchdog.sh
+++ extras/watchdog.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 while true; do
-LOG=/usr/src/pictus/log/dispatcher_in_watchdog.log
+DATE=`/bin/date +%c`
+LOGSTATUS=/usr/src/pictus/log/dispatcher/dispatcher_in_status.log
+LOGERROR=/usr/src/pictus/log/dispatcher/dispatcher_in_error.log
+LOGDEBUG=/usr/src/pictus/log/dispatcher/dispatcher_in_debug.log
 PIDFS=`pidof freeswitch` 
 if [ "$PIDFS" != "" ]; then
-echo "Freeswitch is alive!"
+echo "[$DATE] --FS--"
+echo "[$DATE] --FS--" >> $LOGSTATUS
 else
-echo "Freeswitch is dead!"
-echo "FREESWITCH STOP, STOPPING WATCHER TOO" >> $LOG
+echo "[$DATE] FS CRASH" 
+echo "[$DATE] FS CRASH" >> $LOGERROR
 exit
 fi
-PIDDIS=`ps -ef |grep dispatcher_in.php | grep -v grep | awk '{print $2}'`
+PIDDIS=`ps -ef |grep dispatcher_in.php | grep -v grep | grep -v emacs | awk '{print $2}'`
 if [ "$PIDDIS" != "" ]; then
-echo "Dispatcher is alive!"
+echo "[$DATE] --DIS--"
+echo "[$DATE] --DIS--" >> $LOGSTATUS
 else
-echo "Dispatcher is dead!"
-echo "RESTARTING DISPACHER. WARNING!" >> $LOG
-/usr/bin/php /usr/src/pictus/dispatcher_in/dispatcher_in.php --log=/root/dispatcher_in.log > $LOG 2>&1 &
+echo "[$DATE] DIS CRASH. RESTART" 
+echo "[$DATE] DIS CRASH. RESTART" >> $LOGERROR
+/usr/bin/php /usr/src/pictus/dispatcher_in/dispatcher_in.php --log=$LOGDEBUG > /dev/null 2>&1 &
 fi
 sleep 10
 done

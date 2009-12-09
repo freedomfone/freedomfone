8632339b3ec5a223bbadae7c087bd748efe8b413
Louise <louise@freedomfone.org>
2009-12-09 21:06:15 +0000

    trash bin and log functionality added to refresh method of poll

--- gui/app/models/poll.php
+++ gui/app/models/poll.php
@@ -102,6 +102,127 @@ class Poll extends AppModel{
 
     }
 
+//////////
+
+
+	function refresh(){
+
+      $array = Configure::read('poll_in');
+
+      $instance_id = IID;
+	      
+	       $obj = new ff_event($array);	       
+
+	       if ($obj -> auth != true) {
+
+    	       	  die(printf("Unable to authenticate\r\n"));
+	       }
+
+      	    while ($entry = $obj->getNext('update')){
+
+	        $body 	        =  $entry['Body']; 
+		$_message	=  explode(' ',$body);
+		$polls_code   	=  trim($_message[0]);
+		$votes_chtext 	=  trim($_message[1]);
+		$sender		=  $entry['sender'];
+		$created	=  $entry['Event-Date-Timestamp'];
+		$matched      	=  false;
+
+		// CHECK A: CODE OK (VALID OR INCORRECT)
+		if ($poll_entry = $this->findByCode( $polls_code)){
+		   
+			$poll_id = $poll_entry['Poll']['id'];
+			$status = $poll_entry['Poll']['status'];
+
+			//Fetch valid chtext for identified poll
+	   		foreach ($poll_entry['Vote'] as $key => $vote){
+	
+				//Search for matching chtext
+				if (strcasecmp($votes_chtext,$vote['chtext'])==0){
+		   	   		$matched=true;
+		   		 }
+	 	         }		 
+
+			        //CHECK A1: OPTION CORRECT (correct code, correct chtext)
+
+				if ($matched){
+
+		   	   	  $vote_id = $vote['id'];
+			
+				  switch($status){
+
+				   case 0:
+				   //ADD TO TRASH (pending)
+				   $mode="VALID VOTE, PENDING";
+				   $result = $this->query("insert into bin (instance_id,body,sender,created,mode)values ($instance_id,'$body','$sender','$created','$mode')");
+				   break;
+
+				   case 1:
+				   //ADD TO STATS (open)
+		   	   	   $this->Vote->query("UPDATE votes SET chvotes=chvotes+1 WHERE id=$vote_id "); 
+				   $mode="VALID VOTE, OPEN";
+				   break;
+
+
+				   case 2:
+				   //ADD TO STATS (closed)
+				   $mode="VALID VOTE, CLOSED";
+		   	   	   debug($this->Vote->query("UPDATE votes SET votes_closed=votes_closed+1 WHERE id=$vote_id ")); 
+				   break;
+
+				   }
+
+				} else {
+
+				//CHECK A2: OPTION INCORRECT (correct code, invalid chtext)
+				   switch($status){
+
+				   case 0:
+				   //ADD TO TRASH (pending)
+				   $mode="INCORRECT VOTE, PENDING";
+				   $result = $this->query("insert into bin (instance_id,body,sender,created,mode)values ($instance_id,'$body','$sender','$created','$mode')");
+				   break;
+
+				   case 1:
+				   //ADD TO STATS (open)
+				   $mode="INCORRECT VOTE, OPEN";
+				   $this->query("UPDATE polls SET incorrect_open=incorrect_open+1 WHERE id=$poll_id ");
+				   break;
+
+
+				   case 2:
+				   //ADD TO STATS (closed)
+				   $mode="INCORRECT VOTE, CLOSED";
+				   $this->query("UPDATE polls SET incorrect_closed=incorrect_closed+1 WHERE id=$poll_id");
+				   break;
+
+				   }
+
+	 	         	}
+
+		} else {
+		// CHECK B: CODE NOT OK (INVALID)
+
+			
+			 //ADD TO TRASH
+	   		 $mode="INVALID VOTE";
+			 $result = $this->query("insert into bin (instance_id,body,sender,created,mode)values ($instance_id,'$body','$sender','$created','$mode')");
+	        }
+
+	$this->log("Message: ".$mode."; Body: ".$body."; From: ".$sender."; Timestamp: ".$created, "poll"); 
+
+	}
+
+
+	// Update status of polls (use beforeSave to update status)
+	$data = $this->findAll();
+	foreach ($data as $key => $entry){
+		$this->save($entry);
+	}
+
+
+   }
+
 }
 
 

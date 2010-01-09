b642720ce90c109253b31aedf0357020b01467da
Louise <louise@freedomfone.org>
2010-01-09 22:41:48 +0000

    Improved dispatcher logging

--- dispatcher_in/config.php
+++ dispatcher_in/config.php
@@ -1,14 +1,38 @@
 <?php
-/**
- * Core Configurations
- *
+/****************************************************************************
+ * config.php		- Configuration parameters for incoming dispatcher.
+ * version 		- 1.0.1
  * 
- */
+ * Version: MPL 1.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ *
+ * The Initial Developer of the Original Code is
+ *   Louise Berthilson <louise@it46.se>
+ *
+ *
+ ***************************************************************************/
+
+define("Version", '1.0.1');
 define("ParentXML", "ff-event");
 define("DefaultXSL", "default.xsl");
 define("DirXSL", "templates/");
 define("LogFile", "/tmp/dispatcher_in.log");
 define("LocalDomain", "http://demo.freedomfone.org/freedomfone/");
+define("BaseDir", "/usr/src/pictus/");
+define("ESLPath", BaseDir."/esl/native/ESL.php");
+define("PidFile", BaseDir."freeswitch/scripts/freedomfone/pid/dispatcher_in.pid");
+define("VersionFile", BaseDir."freeswitch/scripts/freedomfone/version/dispatcher_in");
+define("LogLevel", 3);  //1 = low, 2 = medium, 3 = high
 
 $_SocketParam = array(
 	      'host'=>'localhost',
@@ -18,17 +42,12 @@ $_SocketParam = array(
 	      'stream_timeout'=>0.5
 	      );
 
-
 $_DispatcherDB = array(
 	      'host'=>'localhost',
 	      'user'=>'dispatcher_in',
 	      'password'=>'thefone',
 	      'database'=>'spooler_in'
 	      );
-
 $_AllowCURL = array('callback_in' => 'callback_in');
-
 ?>
-
-
 
--- dispatcher_in/templates/message.xsl
+++ dispatcher_in/templates/message.xsl
@@ -2,24 +2,18 @@
 <xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" version="1.0">
 <xsl:output method="xml" indent="yes"/> 
  
-<xsl:template match="ff-event">
-       <ff-event> <xsl:apply-templates select="fields"/> </ff-event>
-
-
+<xsl:template match="/">
+       <event> 
+        <Event-Name><xsl:value-of select="event/headers/Event-Name"/></Event-Name>
+     <headers>
+        <Event-Subclass><xsl:value-of select="event/headers/Event-Subclass"/></Event-Subclass>
+        <FreeSWITCH-IPv4><xsl:value-of select="event/headers/FreeSWITCH-IPv4"/></FreeSWITCH-IPv4>
+        <Event-Date-Timestamp><xsl:value-of select="event/headers/Event-Date-Timestamp"/></Event-Date-Timestamp>
+        <Proto><xsl:value-of select="event/headers/proto"/></Proto>
+        <Login><xsl:value-of select="event/headers/login"/></Login>
+        <From><xsl:value-of select="event/headers/from"/></From>
+       <body><xsl:value-of select="event/body"/></body>
+     </headers>
+       </event> 
 </xsl:template>
- 
-<xsl:template match="fields">
-        <Event-Name><xsl:value-of select="Event-Name"/></Event-Name>
-
-     <fields>
-        <FreeSWITCH-IPv4><xsl:value-of select="FreeSWITCH-IPv4"/></FreeSWITCH-IPv4>
-        <Event-Date-Timestamp><xsl:value-of select="Event-Date-Timestamp"/></Event-Date-Timestamp>
-        <Proto><xsl:value-of select="proto"/></Proto>
-        <Login><xsl:value-of select="login"/></Login>
-        <From><xsl:value-of select="from"/></From>
-        <Body><xsl:value-of select="Body"/></Body>
-     </fields>
-</xsl:template>
-
-
 </xsl:stylesheet>

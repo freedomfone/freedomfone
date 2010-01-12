6ab2d8677f74a1b1ae660700b5fcc4f5f13ad566
louise <louise@a3b35316-8eeb-4143-ac0c-80fe7d8b51ad>
2010-01-12 17:52:28 +0000

    updated spooler db tables

--- dispatcher_in/install/spooler_db_createdb_user.sql
+++ dispatcher_in/install/spooler_db_createdb_user.sql
@@ -12,6 +12,10 @@ GRANT ALL PRIVILEGES ON spooler_in.poll_in TO 'poll_in'@'%' IDENTIFIED BY 'thefo
 GRANT ALL PRIVILEGES ON spooler_in.poll_in TO 'poll_in'@'localhost' IDENTIFIED BY 'thefone' WITH GRANT OPTION;
 GRANT ALL PRIVILEGES ON spooler_in.poll_in TO 'poll_in'@'localhost.localdomain' IDENTIFIED BY 'thefone' WITH GRANT OPTION;
 
+GRANT ALL PRIVILEGES ON spooler_in.bin TO 'bin'@'%' IDENTIFIED BY 'thefone' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON spooler_in.bin TO 'bin'@'localhost' IDENTIFIED BY 'thefone' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON spooler_in.bin TO 'bin'@'localhost.localdomain' IDENTIFIED BY 'thefone' WITH GRANT OPTION;
+
 GRANT ALL PRIVILEGES ON spooler_in.lm_in TO 'lm_in'@'%' IDENTIFIED BY 'thefone' WITH GRANT OPTION;
 GRANT ALL PRIVILEGES ON spooler_in.lm_in TO 'lm_in'@'localhost' IDENTIFIED BY 'thefone' WITH GRANT OPTION;
 GRANT ALL PRIVILEGES ON spooler_in.lm_in TO 'lm_in'@'localhost.localdomain' IDENTIFIED BY 'thefone' WITH GRANT OPTION;
@@ -19,6 +23,15 @@ GRANT ALL PRIVILEGES ON spooler_in.lm_in TO 'lm_in'@'localhost.localdomain' IDEN
 GRANT ALL PRIVILEGES ON spooler_in.callback_in TO 'callback_in'@'%' IDENTIFIED BY 'thefone' WITH GRANT OPTION;
 GRANT ALL PRIVILEGES ON spooler_in.callback_in TO 'callback_in'@'localhost' IDENTIFIED BY 'thefone' WITH GRANT OPTION;
 GRANT ALL PRIVILEGES ON spooler_in.callback_in TO 'callback_in'@'localhost.localdomain' IDENTIFIED BY 'thefone' WITH GRANT OPTION;
+
+GRANT ALL PRIVILEGES ON spooler_in.monitor_ivr TO 'monitor_ivr'@'%' IDENTIFIED BY 'thefone' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON spooler_in.monitor_ivr TO 'monitor_ivr'@'localhost' IDENTIFIED BY 'thefone' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON spooler_in.monitor_ivr TO 'monitor_ivr'@'localhost.localdomain' IDENTIFIED BY 'thefone' WITH GRANT OPTION;
+
+GRANT ALL PRIVILEGES ON spooler_in.cdr TO 'cdr'@'%' IDENTIFIED BY 'thefone' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON spooler_in.cdr TO 'cdr'@'localhost' IDENTIFIED BY 'thefone' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON spooler_in.cdr TO 'cdr'@'localhost.localdomain' IDENTIFIED BY 'thefone' WITH GRANT OPTION;
+
 
 create DATABASE if not exists `spooler_in`;
 flush privileges;

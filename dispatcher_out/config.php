e923d63cfd555b9c537aa634c9613b13c981b779
Louise <louise@freedomfone.org>
2010-01-19 11:44:33 +0000

    Path changes to outgoing dispatcher

--- dispatcher_out/config.php
+++ dispatcher_out/config.php
@@ -10,6 +10,7 @@ define("BasePath", "/usr/src/pictus/");
 define("SpoolerOutDir", "dialer/spooler/incoming/");
 define("SpoolerTmpDir", "/tmp/");
 define("LogFile", "../log/dispatcher_out.log");
+define("PidFile", "/usr/src/pictus/freeswitch/scripts/freedomfone/pid/dispatcher_out.pid");
 
 $_SpoolerOut = array(
 	     'host'  => '127.0.0.1',
@@ -17,6 +18,3 @@ $_SpoolerOut = array(
 
 
 ?>
-
-
-

a530f62de9fdab2afd0b5b5f2273e9ccd2de75ec
Louise <louise@freedomfone.org>
2010-01-19 09:14:04 +0000

    Updates Filters for Audio Bot

--- audio_bot/filters/w2m.sh
+++ audio_bot/filters/w2m.sh
@@ -1,46 +1,62 @@
 #!/bin/bash
 
+# A bidirectional MP3-WAV file converter using lame
+# Freedom Fone 2010
+
+DEBUG=no
+#LAME=/usr/bin/lame
+LAME=/usr/local/freeswitch/bin/lame397
 
 if [ -z "$1" ]; then 
-              echo $0: WAV to MP3 interconverter 
-              echo e.g.:  $0 /usr/local/freedomfone/freeswitch/scripts/freedomfone/leave_message/100/messages/036e08a2-0431-11df-9c1b-bd0d99517a5e.wav
+              echo $0: WAV/MP3-MP3/WAV Audio Converter.   
               exit
-          fi
+fi
 
-LAME=/usr/local/freeswitch/bin/lame397
-#LAME=/usr/bin/lame
-FILENAME_FULL=$1
-FILENAME=$(basename $1)
-EXTENSION=${FILENAME##*.}
-FILE=${FILENAME%.*}
+#Full filename with path
+	FILENAME_FULL=$1
+#Full filename
+	FILENAME=$(basename $1)
+#File extension
+	EXTENSION=${FILENAME##*.}
+#File without extension
+	FILE=${FILENAME%.*}
+
 WEBUSER=www-data
 
+#Debug
 
-#echo $FILENAME_FULL
-#echo $FILENAME
-#echo $EXTENSION
-#echo $FILE
-
+if [ "$DEBUG" == "yes" ]; then
+echo "Full name     :" $FILENAME_FULL
+echo "File name     :" $FILENAME
+echo "File extension:" $EXTENSION
+echo "File root     :" $FILE
+exit 
+fi
 
 if [ "$EXTENSION" == "mp3" ]; then 
 FILEROOT=`echo $FILENAME_FULL|sed 's/\.mp3$//g'`
+
+#File conversion
 $LAME --decode $FILEROOT.mp3 $FILEROOT.wav -S
 
+#Fixing permissions
 /bin/chown $WEBUSER:$WEBUSER $FILEROOT.wav
 /bin/chown $WEBUSER:$WEBUSER $FILEROOT.mp3
-
 exit
+
 fi
 
 if [ "$EXTENSION" == "wav" ]; then 
 FILEROOT=`echo $FILENAME_FULL|sed 's/\.wav$//g'`
+
+#File conversion
 $LAME -V2 $FILEROOT.wav $FILEROOT.mp3 -S
 
+#Fixing permissions
 /bin/chown $WEBUSER:$WEBUSER $FILEROOT.wav
 /bin/chown $WEBUSER:$WEBUSER $FILEROOT.mp3
-
-
 exit
+
 fi
 
-echo "This should not happen!"
+echo "ERROR! File with wrong extension given to filter"

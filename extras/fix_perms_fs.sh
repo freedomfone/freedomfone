a21d8f903a588279dec7d1af6f6d391ac261cc78
Alberto <aep@freedomfone.org>
2010-01-20 18:55:02 +0000

    Allowing www-data to write the conf of leave message

--- extras/fix_perms_fs.sh
+++ extras/fix_perms_fs.sh
@@ -1,12 +1,17 @@
+#Fixing Freeswitch permissions
+chown -R freeswitch:freeswitch /usr/local/freeswitch
+
+#Fixing Freeswitch script permissions
 find /usr/local/freedomfone/freeswitch/scripts -type f | grep -v '\.svn' | xargs chown freeswitch:freeswitch
 find /usr/local/freedomfone/freeswitch/scripts -type f | grep -v '\.svn' | xargs chmod 0644
 find /usr/local/freedomfone/freeswitch/scripts -type f | grep -v '\.svn' | xargs chmod g-s
 find /usr/local/freedomfone/freeswitch/scripts -type d | grep -v '\.svn' | xargs chown freeswitch:freeswitch
 find /usr/local/freedomfone/freeswitch/scripts -type d | grep -v '\.svn' | xargs chmod 0755
-	find /usr/local/freedomfone/freeswitch/scripts -type d | grep -v '\.svn' | xargs chmod g-s
+find /usr/local/freedomfone/freeswitch/scripts -type d | grep -v '\.svn' | xargs chmod g-s
 
 #Folders writable via FS 
 chown -R freeswitch:freeswitch /usr/local/freedomfone/freeswitch/scripts/freedomfone/leave_message/100/messages
+chown -R www-data:www-data /usr/local/freedomfone/freeswitch/scripts/freedomfone/leave_message/100/conf
 
 #Folders writable via GUI
 chown www-data:www-data /usr/local/freedomfone/freeswitch/scripts/freedomfone/leave_message/100/audio_menu

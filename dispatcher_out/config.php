41c5297eba96d6e0a39d2320a499050b3814be6b
Louise <louise@freedomfone.org>
2010-01-20 08:53:02 +0000

    Normalizing paths for Dispatcher OUT

--- dispatcher_out/config.php
+++ dispatcher_out/config.php
@@ -6,11 +6,11 @@
  */
 
 /* FIXME! Move BasePath to globals */
-define("BasePath", "/usr/src/pictus/");
+define("BasePath", "/usr/local/freedomfone");
 define("SpoolerOutDir", "dialer/spooler/incoming/");
 define("SpoolerTmpDir", "/tmp/");
 define("LogFile", "../log/dispatcher_out.log");
-define("PidFile", "/usr/src/pictus/freeswitch/scripts/freedomfone/pid/dispatcher_out.pid");
+define("PidFile", "/usr/local/freedomfone/gui/app/webroot/system/pid/dispatcher_out.pid");
 
 $_SpoolerOut = array(
 	     'host'  => '127.0.0.1',

3d0792c41e9088767cdf0de5fd6b2e4124c09f2f
Alberto <aep@freedomfone.org>
2010-02-23 17:45:25 +0000

    Providing feedback in case that FS is down

--- esl/native/api_client.php
+++ esl/native/api_client.php
@@ -2,6 +2,9 @@
 require_once('ESL.php');
 $command = $argv[1];
 $esl = new eslConnection('127.0.0.1', '8021', 'ClueCon');
-$e = $esl->sendRecv("api $command");
-print $e->getBody();
-
+if ( $esl->connected() ) {
+	$e = $esl->sendRecv("api $command");
+	print $e->getBody(); } 
+	else {
+	echo "Can not connect to Freeswitch\n\n";
+	}

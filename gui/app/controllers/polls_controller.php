ce450a0af9e6907fec6dea2c9738e14272e6ccdc
louise <louise@a3b35316-8eeb-4143-ac0c-80fe7d8b51ad>
2010-05-01 14:45:45 +0000

    Fixed typos in text messages.

--- gui/app/controllers/polls_controller.php
+++ gui/app/controllers/polls_controller.php
@@ -139,7 +139,7 @@ class PollsController extends AppController{
      	     $title = $this->Poll->getTitle($id);
     	     if($this->Poll->delete($id,true))
 	     {
-    	     $this->Session->setFlash(__('The follwing poll has been deleted',true).': '.$title, 'default', array('class'=>'message_success'));
+    	     $this->Session->setFlash(__('The following poll has been deleted',true).': '.$title, 'default', array('class'=>'message_success'));
 	     $this->log('Msg: POLL DELETED; Id:title: '.$id.":".$title, 'poll');	
 	     $this->redirect(array('action' => '/index'));
 

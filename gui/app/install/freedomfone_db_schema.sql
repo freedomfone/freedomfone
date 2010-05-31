8609fb76de2f45ae531a3c880fb944fa97d1f86c
louise <louise@a3b35316-8eeb-4143-ac0c-80fe7d8b51ad>
2010-05-31 20:21:20 +0000

    Update db schema

--- gui/app/install/freedomfone_db_schema.sql
+++ gui/app/install/freedomfone_db_schema.sql
@@ -461,6 +461,13 @@ CREATE TABLE `processes` (
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 SET character_set_client = @saved_cs_client;
 
+LOCK TABLES `processes` WRITE;
+/*!40000 ALTER TABLE `processes` DISABLE KEYS */;
+INSERT INTO `processes` VALUES (1,'dispatcher_in',0,'dispatcher_in/dispatcherESL.php --log=/var/tmp/dispatcher_in.log > /dev/null 2>&1 & echo $!',100,'Incoming dispatcher',0,0,'','run','/usr/bin/php'),(2,'dispatcher_out',0,'dispatcher_out/dispatcher_out.php > /dev/null 2>&1 & echo $!',100,'Outgoing dispatcher',0,0,'','run','/usr/bin/php'),(3,'dispatcher_in_version',NULL,'dispatcher_in/dispatcherESL.php -V',100,'Incoming dispatcher version',0,0,NULL,'version','/usr/bin/php');
+/*!40000 ALTER TABLE `processes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
 --
 -- Table structure for table `settings`
 --

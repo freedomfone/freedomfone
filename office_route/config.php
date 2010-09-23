fff725045a005f4ca6c074d8885590f2154166eb
aep <aep@a3b35316-8eeb-4143-ac0c-80fe7d8b51ad>
2010-09-23 12:07:54 +0000

    Added support for multiple SIM cards

--- office_route/config.php
+++ office_route/config.php
@@ -2,6 +2,8 @@
 
 define("BaseDir", "/usr/local/freedomfone/");
 define("ESLPath", BaseDir."/esl/native/ESL.php");
+define("LogFile", "/tmp/pop3_daemon.log");
+define("LogLevel", 3);  //1 = low, 2 = medium, 3 = high
 
 $_SocketParam = array(
 	      'host'=>'localhost',
@@ -10,6 +12,40 @@ $_SocketParam = array(
 	      'timeout'=>3,
 	      'stream_timeout'=>0.5
 	      );
+
+$_OfficerouteParam1 = array(
+			'host'=>'192.168.46.109',
+	      		'user'=>'admin',
+	      		'pass'=>'2n',
+	      		'port'=>'110'
+	      		);
+
+
+$_OfficerouteParam = array(
+		   array(
+			'host'=>'192.168.46.109',
+	      		'user'=>'Admin',
+	      		'pass'=>'2n',
+	      		'port'=>'110'
+	      		),
+		   array(
+			'host'=>'192.168.46.109',
+	      		'user'=>'101',
+	      		'pass'=>'2n',
+	      		'port'=>'110'
+	      		),
+		   array(
+			'host'=>'192.168.46.109',
+	      		'user'=>'102',
+	      		'pass'=>'2n',
+	      		'port'=>'110'
+	      		),
+		   array(
+			'host'=>'192.168.46.109',
+	      		'user'=>'103',
+	      		'pass'=>'2n',
+	      		'port'=>'110'
+	      		));
 
 
 

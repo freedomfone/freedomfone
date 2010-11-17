01ba39a121662ed99f16c546254612faabcb2d88
Louise <louise@freedomfone.org>
2010-11-17 17:38:29 +0000

    Added Javascript to create custom SMS to trunk

--- freeswitch/scripts/freedomfone/sms/main.js
+++ freeswitch/scripts/freedomfone/sms/main.js
@@ -1,9 +1,13 @@
 var sms = argv[0];
 var sender = argv[1];
 var receiver = argv[2];
-    e = new Event("custom", "message");
+var date = argv[3];
+
+console_log(" [ " + sms + " ] " + sender +"\n");
+    e = new Event("custom", "officeroute");
     e.addHeader("FF-SMS-Sender-Number",sender);
     e.addHeader("FF-SMS-Receiver-Number",receiver);
+    e.addHeader("proto","GSM");
+    e.addHeader("epoch",date);
     e.addBody(sms);
     e.fire();
-

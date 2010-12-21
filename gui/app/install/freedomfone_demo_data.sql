3d281f29ad05c168664164f32a053796d8dbb579
louise <louise@a3b35316-8eeb-4143-ac0c-80fe7d8b51ad>
2010-12-21 11:14:14 +0000

    Adding sql script for adding demo data

---
+++ gui/app/install/freedomfone_demo_data.sql
@@ -0,0 +1,7 @@
+INSERT INTO `ivr_menus` VALUES (1,100,'Cholera','','','','','1. Welcome. cholera_menu_welcome.wav','2. Instructions. cholera_menu_options_160610_final.wav','3. Goodbye. cholera_goodbye.wav','4. Invalid. cholera_invalid_selection.wav',1,'node',2,'node',3,'node',4,'node',NULL,'lam',NULL,'node',NULL,'node',NULL,'node',NULL,NULL,1,1292924823,1292927669,0,0,0,0);
+
+
+INSERT INTO `nodes` VALUES (1,100,'Cholera 1','1292926356_Press_1._cholera_menu_ch_1_symptoms_and_prevention',128,1292926360,1292926360),(2,100,'Cholera 2','1292926376_Press_2._cholera_menu_ch_2_treatment',104,1292926380,1292926380),(3,100,'Cholera 3','1292926427_Press_3._cholera_menu_ch_3_where_to_go',41,1292926431,1292926431),(4,100,'Cholera 4','1292926445_Press_4._cholera_menu_ch_4_feature_tsitsi_unicef',59,1292926449,1292926449);
+
+
+INSERT INTO `messages` VALUES (1,'55566677','No title',0,'efb25e98-0ce6-11e0-b030-e123b6921c64',NULL,1292924705,0,'http%3A//192.168.1.10/freedomfone16/freedomfone/leave_message/100/messages/',1,1,19,0,NULL,NULL),(2,'55566677','No title',0,'3afa7f20-0ce7-11e0-b033-e123b6921c64',NULL,1292924825,0,'http%3A//192.168.1.10/freedomfone16/freedomfone/leave_message/100/messages/',1,1,19,0,NULL,NULL);

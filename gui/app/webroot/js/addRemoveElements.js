ed7dcb468ec37141f8f3d6a50e392b62b959fb98
louise <louise@a3b35316-8eeb-4143-ac0c-80fe7d8b51ad>
2010-12-28 10:38:39 +0000

    New CSS to Poll Javascript

--- gui/app/webroot/js/addRemoveElements.js
+++ gui/app/webroot/js/addRemoveElements.js
@@ -41,7 +41,7 @@ Event.add(window, 'load', function() {
 
 	Event.add('add-element', 'click', function() {
 		var el = document.createElement('p');
-				el.innerHTML = '<table><tr><td>Option </td><td><input type="text" id = "Vote'+ i +'Chtext value= "" name="data[Vote][][chtext]"></td></tr></table>';
+				el.innerHTML = '<table cellspacing=0 class="blue"><tr class="blue"><td>Option </td><td><input type="text" id = "Vote'+ i +'Chtext value= "" name="data[Vote][][chtext]"></td></tr></table>';
 
 
 	Dom.add(el, 'content');

92a6a8d08307a3b338fdb3f0c5bb497bd16b0034
louise <louise@a3b35316-8eeb-4143-ac0c-80fe7d8b51ad>
2010-12-29 19:22:15 +0000

    Update demo script for sekowei

--- installer/sekowei/6_sekowei_demo_data.sh
+++ installer/sekowei/6_sekowei_demo_data.sh
@@ -7,7 +7,7 @@ SVNROOT=/usr/local/freedomfone
 
 DEMODIR=/usr/local/freedomfone/installer/sekowei/demo
 
-cp $DEMODIR/ivr_nodes/* $SVNROOT/gui/app/webroot/freedomfone/ivr/100/nodes/
+cp $DEMODIR/ivr_nodes/* $SVNROOT/gui/app/webroot/freedomfone/ivr/nodes/
 cp $DEMODIR/ivr_ivr/*   $SVNROOT/gui/app/webroot/freedomfone/ivr/100/ivr/
 cp $DEMODIR/lam_ivr/*   $SVNROOT/gui/app/webroot/freedomfone/leave_message/100/audio_menu/
 cp $DEMODIR/lam_inbox/* $SVNROOT/gui/app/webroot/freedomfone/leave_message/100/messages/

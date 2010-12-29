c69f308b740293a55e021944162dd1bb8c76c4af
aep <aep@a3b35316-8eeb-4143-ac0c-80fe7d8b51ad>
2010-12-29 21:00:49 +0000

    Added default ivr.xml for demo IVR

--- installer/sekowei/6_sekowei_demo_data.sh
+++ installer/sekowei/6_sekowei_demo_data.sh
@@ -8,7 +8,9 @@ SVNROOT=/usr/local/freedomfone
 DEMODIR=/usr/local/freedomfone/installer/sekowei/demo
 
 cp $DEMODIR/ivr_nodes/* $SVNROOT/gui/app/webroot/freedomfone/ivr/nodes/
-cp $DEMODIR/ivr_ivr/*   $SVNROOT/gui/app/webroot/freedomfone/ivr/100/ivr/
+cp $DEMODIR/ivr_ivr/*.mp3   $SVNROOT/gui/app/webroot/freedomfone/ivr/100/ivr/
+cp $DEMODIR/ivr_ivr/*.wav   $SVNROOT/gui/app/webroot/freedomfone/ivr/100/ivr/
+cp $DEMODIR/ivr_ivr/ivr.xml   $SVNROOT/gui/app/webroot/freedomfone/ivr/100/conf/
 cp $DEMODIR/lam_ivr/*   $SVNROOT/gui/app/webroot/freedomfone/leave_message/100/audio_menu/
 cp $DEMODIR/lam_inbox/* $SVNROOT/gui/app/webroot/freedomfone/leave_message/100/messages/
 

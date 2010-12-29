8e94b90c3696d2d549bab1a29f32cc6e246255ed
Alberto <aep@freedomfone.org>
2010-12-29 18:15:33 +0000

    Sekowei installer

---
+++ installer/sekowei/6_sekowei_demo_data.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#Script to add demo data
+
+SVNROOT=/usr/local/freedomfone
+
+/usr/bin/mysql -u root -p freedomfone < $SVNROOT/gui/app/install/freedomfone_demo_data.sql
+
+DEMODIR=/usr/local/freedomfone/installer/sekowei/demo
+
+cp $DEMODIR/ivr_nodes/* $SVNROOT/gui/app/webroot/freedomfone/ivr/100/nodes/
+cp $DEMODIR/ivr_ivr/*   $SVNROOT/gui/app/webroot/freedomfone/ivr/100/ivr/
+cp $DEMODIR/lam_ivr/*   $SVNROOT/gui/app/webroot/freedomfone/leave_message/100/audio_menu/
+cp $DEMODIR/lam_inbox/* $SVNROOT/gui/app/webroot/freedomfone/leave_message/100/messages/
+
+

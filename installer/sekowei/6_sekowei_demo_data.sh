e1e246ce9e7888e26a13e2b48e3531e225c5111f
louise <louise@a3b35316-8eeb-4143-ac0c-80fe7d8b51ad>
2011-01-31 14:32:28 +0000

    Fixed permission to demo data, updated demo ivr.xml with service id

--- installer/sekowei/6_sekowei_demo_data.sh
+++ installer/sekowei/6_sekowei_demo_data.sh
@@ -10,8 +10,9 @@ DEMODIR=/usr/local/freedomfone/installer/sekowei/demo
 cp $DEMODIR/ivr_nodes/* $SVNROOT/gui/app/webroot/freedomfone/ivr/nodes/
 cp $DEMODIR/ivr_ivr/*.mp3   $SVNROOT/gui/app/webroot/freedomfone/ivr/100/ivr/
 cp $DEMODIR/ivr_ivr/*.wav   $SVNROOT/gui/app/webroot/freedomfone/ivr/100/ivr/
+cp $DEMODIR/ivr_ivr/ivr_full.xml   $SVNROOT/gui/app/webroot/xml_curl/ivr.xml
 cp $DEMODIR/ivr_ivr/ivr.xml   $SVNROOT/gui/app/webroot/freedomfone/ivr/100/conf/
 cp $DEMODIR/lam_ivr/*   $SVNROOT/gui/app/webroot/freedomfone/leave_message/100/audio_menu/
 cp $DEMODIR/lam_inbox/* $SVNROOT/gui/app/webroot/freedomfone/leave_message/100/messages/
-
+bash $SVNROOT/extras/fix_perms_gui.sh
 

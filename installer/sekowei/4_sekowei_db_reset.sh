ad68f414d1ef5089ff8d98abd07f09097c47dc52
Alberto <aep@freedomfone.org>
2011-02-10 15:27:56 +0000

    clean audio files and update 4_ reset script

--- installer/sekowei/4_sekowei_db_reset.sh
+++ installer/sekowei/4_sekowei_db_reset.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #Script to reset the DB
-
+find /usr/local/freedomfone/freeswitch/scripts -type f -name *wav -print0 | xargs -0 rm -f 
+find /usr/local/freedomfone/freeswitch/scripts -type f -name *mp3 -print0 | xargs -0 rm -f
 SVNROOT=/usr/local/freedomfone
 /usr/bin/mysql -u root -p < $SVNROOT/gui/app/install/freedomfone_db_createdb_user.sql  
 /usr/bin/mysql -u root -p freedomfone < $SVNROOT/gui/app/install/freedomfone_db_schema.sql

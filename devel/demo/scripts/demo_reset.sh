0757273e9fecd1c48e5d29219f2768677c98495b
aep <aep@a3b35316-8eeb-4143-ac0c-80fe7d8b51ad>
2011-02-11 08:35:59 +0000

    Deleting devel folder from release trunk

--- devel/demo/scripts/demo_reset.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd /var/www/freedomfone/app/webroot/defaults/lm/audio_menu/
-cp *.mp3 /var/www/freedomfone/app/webroot/freedomfone/leave_message/100/audio_menu/
-cp *.wav /var/www/freedomfone/app/webroot/freedomfone/leave_message/100/audio_menu/

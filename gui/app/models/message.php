3eacd75005328cec45f5c4da77327b5359fab13f
louise <louise@a3b35316-8eeb-4143-ac0c-80fe7d8b51ad>
2011-05-11 15:55:14 +0000

    Urldecode sender from Mobigater

--- gui/app/models/message.php
+++ gui/app/models/message.php
@@ -74,7 +74,7 @@ class Message extends AppModel {
 	       $value = $entry['FF-CallerName'];
 
 
-	       $data= array ( 'sender'  =>$entry['FF-CallerID'],
+	       $data= array ( 'sender'  => urldecode($entry['FF-CallerID']),
 	       	      	      'file'    =>$entry['FF-FileID'],
 	       	      	      'created' =>$created,
 			      'length'  =>$length,

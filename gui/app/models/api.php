df73cbd043600fe2197a42dcb6b9693999f48b36
Louise <louise@freedomfone.org>
2011-06-08 18:38:04 +0000

    API methods

--- gui/app/models/api.php
+++ gui/app/models/api.php
@@ -29,6 +29,152 @@ class Api extends AppModel{
       var $useTable = false;
 
 
+/*
+ * Validation of POST data
+ *
+ * @params
+ *      $post_vars(array)
+ *      $keys(array)
+ *
+ * @return 
+ *      boolean
+ */
+  function validPostVars($post_vars,$keys){
+
+           $status = true;
+
+           foreach($post_vars as $key => $var){
+
+             if(!in_array($key,$keys)){
+                $status = false;
+             
+             }
+                              
+           }
+           return $status;
+
+  }
+
+/*
+ * Validation of integer range
+ *
+ * @params
+ *      $data(int) data to be within min and max
+ *      $min(int)
+ *      $max(int)
+ *
+ * @return 
+ *      boolean
+ */
+      function validRange($data, $min, $max){
+
+               if($data >= $min  && $data <= $max){
+                          return true;
+                          } else {
+
+                          return false;
+                          }
+
+      }
+
+
+
+/*
+ * Add Leave-a-message mp3 files to object
+ *
+ * @params
+ *      $lm_menu(array) 
+ *
+ * @return 
+ *      $lm_menu(array)
+ */
+    
+    function addLmMenuFiles($lm_menu){
+
+           $lm_settings = Configure::read('LM_SETTINGS');
+           foreach($lm_menu as $key => $entry) {
+
+               $path = $lm_settings['path'].$entry['LmMenu']['instance_id']."/".$lm_settings['dir_menu'];
+
+               if(file_exists($path.'lmWelcome.mp3')){
+                 $lm_menu[$key]['LmMenu']['lmWelcome'] = urlencode($path.'lmWelcome.mp3');
+               } else {
+                 $lm_menu[$key]['LmMenu']['lmWelcome'] = false;
+               }
+
+
+               if(file_exists($path.'lmInform.mp3')){
+                 $lm_menu[$key]['LmMenu']['lmInform'] = urlencode($path.'lmInform.mp3');
+               } else {
+                 $lm_menu[$key]['LmMenu']['lmInform'] = false;
+               }
+
+               if(file_exists($path.'lmInvalid.mp3')){
+                 $lm_menu[$key]['LmMenu']['lmInvalid'] = urlencode($path.'lmInvalid.mp3');
+               } else {
+                 $lm_menu[$key]['LmMenu']['lmInvalid'] = false;
+               }
+
+               if(file_exists($path.'lmLong.mp3')){
+                 $lm_menu[$key]['LmMenu']['lmLong'] = urlencode($path.'lmLong.mp3');
+               } else {
+                 $lm_menu[$key]['LmMenu']['lmLong'] = false;
+               }
+
+               if(file_exists($path.'lmSelect.mp3')){
+                 $lm_menu[$key]['LmMenu']['lmSelect'] = urlencode($path.'lmSelect.mp3');
+               } else {
+                 $lm_menu[$key]['LmMenu']['lmSelect'] = false;
+               }
+
+               if(file_exists($path.'lmDelete.mp3')){
+                 $lm_menu[$key]['LmMenu']['lmDelete'] = urlencode($path.'Delete.mp3');
+               } else {
+                 $lm_menu[$key]['LmMenu']['lmDelete'] = false;
+               }
+
+               if(file_exists($path.'lmSave.mp3')){
+                 $lm_menu[$key]['LmMenu']['lmSave'] = urlencode($path.'lmSave.mp3');
+               } else {
+                 $lm_menu[$key]['LmMenu']['lmSave'] = false;
+               }
+
+               if(file_exists($path.'lmGoodbye.mp3')){
+                 $lm_menu[$key]['LmMenu']['lmGoodbye'] = urlencode($path.'lmGoodbye.mp3');
+               } else {
+                 $lm_menu[$key]['LmMenu']['lmGoodbye'] = false;
+               }
+
+           }
+ 
+           return $lm_menu;
+       }
+
+/*
+ * Get mp3 file to Message
+ *
+ * @params
+ *      $filename(string)
+ *      $instance_id(id) 
+ *
+ * @return 
+ *      $path(string)
+ */
+    
+    function getMessageAudio($filename,$instance_id){
+
+           $ext  ='mp3';
+           $lm_settings = Configure::read('LM_SETTINGS');
+           $path = $lm_settings['path'].$instance_id."/".$lm_settings['dir_messages'];
+
+               if(file_exists($file = $path.$filename.'.'.$ext)){
+                 return urlencode($file);
+               } else {
+                 return __('No file found',true);
+               }
+
+    }
+
 }
 
 ?>

4883e3833e8d57351c6e41d739890d3ea8f1bc57
louise <louise@a3b35316-8eeb-4143-ac0c-80fe7d8b51ad>
2011-09-16 10:08:55 +0000

    3.0 migration patches

--- gui/app/config/database.php
+++ gui/app/config/database.php
@@ -1,5 +1,4 @@
 <?php
-/* SVN FILE: $Id: database.php.default 8004 2009-01-16 20:15:21Z gwoo $ */
 /**
  * This is core configuration file.
  *
@@ -7,22 +6,18 @@
  *
  * PHP versions 4 and 5
  *
- * CakePHP(tm) :  Rapid Development Framework (http://www.cakephp.org)
- * Copyright 2005-2008, Cake Software Foundation, Inc. (http://www.cakefoundation.org)
+ * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
+ * Copyright 2005-2011, Cake Software Foundation, Inc. (http://cakefoundation.org)
  *
  * Licensed under The MIT License
  * Redistributions of files must retain the above copyright notice.
  *
- * @filesource
- * @copyright     Copyright 2005-2008, Cake Software Foundation, Inc. (http://www.cakefoundation.org)
- * @link          http://www.cakefoundation.org/projects/info/cakephp CakePHP(tm) Project
+ * @copyright     Copyright 2005-2011, Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @link          http://cakephp.org CakePHP(tm) Project
  * @package       cake
  * @subpackage    cake.app.config
  * @since         CakePHP(tm) v 0.2.9
- * @version       $Revision: 8004 $
- * @modifiedby    $LastChangedBy: gwoo $
- * @lastmodified  $Date: 2009-01-16 12:15:21 -0800 (Fri, 16 Jan 2009) $
- * @license       http://www.opensource.org/licenses/mit-license.php The MIT License
+ * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
  */
 /**
  * In this file you set up your database connection details.
@@ -88,4 +83,5 @@ class DATABASE_CONFIG {
 		'prefix' => '',
 	);
 }
+
 ?>

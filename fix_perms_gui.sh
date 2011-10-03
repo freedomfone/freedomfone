05c29e7bacf68f02535ffe5a4cb173d4059a4593
aep <aep@a3b35316-8eeb-4143-ac0c-80fe7d8b51ad>
2011-10-03 15:52:12 +0000

    New installer info and scripts

--- fix_perms_gui.sh
+++ fix_perms_gui.sh
@@ -1,3 +1,5 @@
+chown -Rf www-data:www-data /opt/freedomfone/log
+chmod 770 /opt/freedomfone/log
 chown -Rf www-data:www-data /opt/freedomfone/gui/
 find /opt/freedomfone/gui/ -type f | grep -v '\.svn' | xargs chmod 640 
 find /opt/freedomfone/gui/ -type d | grep -v '\.svn' | xargs chmod 750 

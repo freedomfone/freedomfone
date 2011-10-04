19a2e3e618ac5c7bfc459755c0fe82f06f21bf90
aep <aep@a3b35316-8eeb-4143-ac0c-80fe7d8b51ad>
2011-10-04 08:28:41 +0000

    freeswitch:www-data perms

--- fix_perms_gui.sh
+++ fix_perms_gui.sh
@@ -1,5 +1,5 @@
 chown -Rf www-data:www-data /opt/freedomfone/log
 chmod 770 /opt/freedomfone/log
 chown -Rf freeswitch:www-data /opt/freedomfone/gui/
-find /opt/freedomfone/gui/ -type f | grep -v '\.svn' | xargs chmod 640 
-find /opt/freedomfone/gui/ -type d | grep -v '\.svn' | xargs chmod 750 
+find /opt/freedomfone/gui/ -type f | grep -v '\.svn' | xargs chmod 660 
+find /opt/freedomfone/gui/ -type d | grep -v '\.svn' | xargs chmod 770 

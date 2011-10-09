a7a56295c52418097dae1f110cf1b3329b408f8a
aep <aep@a3b35316-8eeb-4143-ac0c-80fe7d8b51ad>
2011-10-09 16:23:41 +0000

    Build 9

--- download_bin.sh
+++ download_bin.sh
@@ -3,9 +3,9 @@ REPO=http://www.it46.se/freedomfone/manguensis/
 DOWNLOAD=/opt/freedomfone/
 CEPSTRAL=Cepstral_Allison-8kHz_i386-linux_5.1.0.tar.gz
 BUILD=9
-FREESWITCH=freeswitch_1.0.head-git.master.20110530.ffone-$BUILD_i386.deb
-FREESWITCH_LANG=freeswitch-lang-en_1.0.head-git.master.20110530.ffone-$BUILD_i386.deb
-FREESWITCH_SPIDERMONKEY=freeswitch-spidermonkey_1.0.head-git.master.20110530.ffone-$BUILD_i386.deb
+FREESWITCH=freeswitch_1.0.head-git.master.20110530.ffone-"$BUILD"_i386.deb
+FREESWITCH_LANG=freeswitch-lang-en_1.0.head-git.master.20110530.ffone-"$BUILD"_i386.deb
+FREESWITCH_SPIDERMONKEY=freeswitch-spidermonkey_1.0.head-git.master.20110530.ffone-"$BUILD"_i386.deb
 
 for i in $CEPSTRAL $FREESWITCH $FREESWITCH_LANG $FREESWITCH_SPIDERMONKEY; do
 

11666730178eb3a6ed92ea6d0139dc091b9b5641
Alberto <aep@freedomfone.org>
2011-10-12 06:17:12 +0000

    l10n tree

---
+++ l10n/migrate.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+# Freedom Fone 2011
+# Migrates translations between release using translate-toolkit 
+#
+#################################################################
+
+FROM=sekowei
+#TO=manguensis
+TO=suyo
+LANG="swa fre esp"
+for i in $LANG; do
+echo "Migrating $i"
+pomigrate2 $FROM"/"$i"/LC_MESSAGES" $TO"/"$i"/LC_MESSAGES" "pot/"$TO
+pocount $TO"/"$i"/LC_MESSAGES/default.po"
+done

8aead803b6e369ff09331c73bb85e0cfd9da1a7d
aep <aep@a3b35316-8eeb-4143-ac0c-80fe7d8b51ad>
2011-11-07 17:52:34 +0000

    Build 10: fixes LAM length bug

--- download_bin.sh
+++ download_bin.sh
@@ -2,7 +2,7 @@
 REPO=http://www.it46.se/freedomfone/manguensis/
 DOWNLOAD=/opt/freedomfone/
 CEPSTRAL=Cepstral_Allison-8kHz_i386-linux_5.1.0.tar.gz
-BUILD=9
+BUILD=10
 FREESWITCH=freeswitch_1.0.head-git.master.20110530.ffone-"$BUILD"_i386.deb
 FREESWITCH_LANG=freeswitch-lang-en_1.0.head-git.master.20110530.ffone-"$BUILD"_i386.deb
 FREESWITCH_SPIDERMONKEY=freeswitch-spidermonkey_1.0.head-git.master.20110530.ffone-"$BUILD"_i386.deb

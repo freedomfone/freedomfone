24e047b6cb05b9a9413ce5f103471d2d7f4e5dfd
aep <aep@a3b35316-8eeb-4143-ac0c-80fe7d8b51ad>
2012-01-31 08:34:33 +0000

    l10n of config message

--- gui/app/config/config.php
+++ gui/app/config/config.php
@@ -153,10 +153,10 @@ $config['EXT_MAPPING'] = array(
 		      );
 
 $config['LANGUAGES'] = array(
-                            'eng' => __('English',true),
-                            'swa' => __('Swahili',true),
-                            'esp' => __('Spanish',true)
-                                                  );
+                            'eng' => 'English',
+                            'swa' => 'Swahili',
+                            'esp' => 'Spanish',
+                            );
 
 $config['RSS']	= array(
 	      'path' =>'http://freedomfone.org/updates.xml',

10d81ba6be26045cd3cf67c28c030b9bc0e848ac
aep <aep@a3b35316-8eeb-4143-ac0c-80fe7d8b51ad>
2012-02-02 14:22:02 +0000

    Deleting old build system

--- esl/native/samples/test.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once('ESL.php');
-
-$esl = new eslConnection('127.0.0.1', '8021', 'ClueCon');
-$e = $esl->sendRecv("api status");
-print $e->getBody();
-
--- freeswitch/scripts/freedomfone/echo/main.js
+++
@@ -1,2 +0,0 @@
-var echoVar = argv[0];
-console_log("The echo is: " + echoVar + "\n\n");

6cee26a06b442ea9ae8dad55e16953ba17193630
aep <aep@a3b35316-8eeb-4143-ac0c-80fe7d8b51ad>
2012-03-08 20:36:17 +0000

    Locale push script

---
+++ locale_push.sh
@@ -0,0 +1,4 @@
+cp l10n/suyo/esp/LC_MESSAGES/default.po config/locale/esp/LC_MESSAGES/
+cp l10n/suyo/fre/LC_MESSAGES/default.po config/locale/fre/LC_MESSAGES/
+cp l10n/suyo/swa/LC_MESSAGES/default.po config/locale/swa/LC_MESSAGES/
+

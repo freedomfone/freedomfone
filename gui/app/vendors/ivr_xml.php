6779fc83b139be90d521efd091e9c216f483a27c
Alberto <aep@freedomfone.org>
2012-10-18 12:57:07 +0000

    Fixing Building to work with Freeswitch 1.2 deb packages

--- gui/app/vendors/ivr_xml.php
+++ gui/app/vendors/ivr_xml.php
@@ -110,7 +110,7 @@ public $ext;
 
 
      $ivr_default = Configure::read('IVR_DEFAULT');
-     $this->menu_path	   = '$${base_dir}/scripts/'.$this->ivr_path.$data['instance_id'].'/'.$this->ivr_dir_menu;
+     $this->menu_path	   = '$${ff_base_dir}/scripts/'.$this->ivr_path.$data['instance_id'].'/'.$this->ivr_dir_menu;
 
 	  
 	   $comment	    = "type: ".$data['ivr_type'];

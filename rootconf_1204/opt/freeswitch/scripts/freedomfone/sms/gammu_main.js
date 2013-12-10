d9e72344be228f44895d2add19a2e6cea6cbbc3b
Louise <louise@freedomfone.org>
2013-12-10 21:57:26 +0000

    Adding Javascript to generate ESL event for incoming Gammu based SMS

---
+++ rootconf_1204/opt/freeswitch/scripts/freedomfone/sms/gammu_main.js
@@ -0,0 +1,15 @@
+var sms = argv[0];
+var sender = argv[1];
+var receiver = argv[2];
+var hw_unit = argv[3];
+var date = argv[4];
+
+console_log(" [ " + sms + " ] " + sender +"\n");
+    e = new Event("custom", "gammu");
+    e.addHeader("FF-SMS-Sender-Number",sender);
+    e.addHeader("FF-SMS-Receiver-Number",receiver);
+    e.addHeader("FF-Hardware-Unit",hw_unit);
+    e.addHeader("proto","GSM");
+    e.addHeader("epoch",date);
+    e.addBody(sms);
+    e.fire();

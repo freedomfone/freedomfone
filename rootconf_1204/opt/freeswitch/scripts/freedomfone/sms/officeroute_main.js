26a5dd21ca665098a5e73f63ffaefd721fde51d8
Alberto <aep@freedomfone.org>
2013-12-11 16:15:18 +0000

    Full header event

--- rootconf_1204/opt/freeswitch/scripts/freedomfone/sms/officeroute_main.js
+++ rootconf_1204/opt/freeswitch/scripts/freedomfone/sms/officeroute_main.js
@@ -1,12 +1,14 @@
 var sms = argv[0];
 var sender = argv[1];
 var receiver = argv[2];
-var date = argv[3];
+var hw_unit = argv[3];
+var date = argv[4];
 
 console_log(" [ " + sms + " ] " + sender +"\n");
     e = new Event("custom", "officeroute");
     e.addHeader("FF-SMS-Sender-Number",sender);
     e.addHeader("FF-SMS-Receiver-Number",receiver);
+    e.addHeader("FF-Hardware-Unit",hw_unit);
     e.addHeader("proto","GSM");
     e.addHeader("epoch",date);
     e.addBody(sms);

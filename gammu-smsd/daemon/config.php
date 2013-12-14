775b506d2dd1cb3d0a6c8505c39dfa952640d3d8
Alberto <aep@freedomfone.org>
2013-12-14 22:38:06 +0000

    Cleanup

--- gammu-smsd/daemon/config.php
+++ gammu-smsd/daemon/config.php
@@ -1,8 +1,6 @@
 <?php
 
 define("LogFile", "/opt/freedomfone/log/gammu_daemon.log");
-define("GammuConfig", "/tmp/gammu.rc");
-define("GammuDiscovery", "/tmp/gammu-discovery");
 define("LogLevel", 3);  //1 = low, 2 = medium, 3 = high
 define("BaseDir", "/opt/freedomfone/");
 define("ESLPath", BaseDir."/esl/native/ESL.php");

791a50f8975f73f3d4ddc83eb5b16f4b1c426ef0
Alberto <aep@freedomfone.org>
2013-12-15 15:57:18 +0000

    Test for inbox gammu

---
+++ gammu-smsd/daemon/tests/bin_test.sql
@@ -0,0 +1,12 @@
+use freedomfone;
+DELETE FROM bin;
+use gammu;
+INSERT INTO inbox (ReceivingDateTime, Text, SenderNumber, Coding, SMSCNumber, UDH, Class, TextDecoded, RecipientID) VALUES ('2013-06-19 22:02:30 GMT', 'D83DDC4D', '+460000001', 'Unicode_No_Compression', '32475161616', '', -1, 'TEXT SMS', 'OR1-240016010774781');
+INSERT INTO inbox (ReceivingDateTime, Text, SenderNumber, Coding, SMSCNumber, UDH, Class, TextDecoded, RecipientID) VALUES ('2013-06-19 22:02:32 GMT', 'D83DDC4D', '+460000001', 'Unicode_No_Compression', '32475161616', '', -1, 'TEXT SMS', 'OR1-240016010774781');
+INSERT INTO inbox (ReceivingDateTime, Text, SenderNumber, Coding, SMSCNumber, UDH, Class, TextDecoded, RecipientID) VALUES ('2013-06-19 22:02:33 GMT', 'D83DDC4D', '+460000001', 'Unicode_No_Compression', '32475161616', '', -1, 'TEXT SMS', 'OR2-240016010774782');
+INSERT INTO inbox (ReceivingDateTime, Text, SenderNumber, Coding, SMSCNumber, UDH, Class, TextDecoded, RecipientID) VALUES ('2013-06-19 22:02:34 GMT', 'D83DDC4D', '+460000001', 'Unicode_No_Compression', '32475161616', '', -1, 'TEXT SMS', 'OR2-240016010774782');
+INSERT INTO inbox (ReceivingDateTime, Text, SenderNumber, Coding, SMSCNumber, UDH, Class, TextDecoded, RecipientID) VALUES ('2013-06-19 22:02:35 GMT', 'D83DDC4D', '+460000001', 'Unicode_No_Compression', '32475161616', '', -1, 'TEXT SMS', 'OR2-240016010774782');
+INSERT INTO inbox (ReceivingDateTime, Text, SenderNumber, Coding, SMSCNumber, UDH, Class, TextDecoded, RecipientID) VALUES ('2013-06-19 22:02:35 GMT', 'D83DDC4D', '+460000001', 'Unicode_No_Compression', '32475161616', '', -1, 'TEXT SMS', 'GM1-240016010774782');
+INSERT INTO inbox (ReceivingDateTime, Text, SenderNumber, Coding, SMSCNumber, UDH, Class, TextDecoded, RecipientID) VALUES ('2013-06-19 22:02:35 GMT', 'D83DDC4D', '+460000001', 'Unicode_No_Compression', '32475161616', '', -1, 'TEXT SMS', 'GM2-240016010774784');
+INSERT INTO inbox (ReceivingDateTime, Text, SenderNumber, Coding, SMSCNumber, UDH, Class, TextDecoded, RecipientID) VALUES ('2013-06-19 22:02:35 GMT', 'D83DDC4D', '+460000001', 'Unicode_No_Compression', '32475161616', '', -1, 'TEXT SMS', 'GM3-240016010774785');
+

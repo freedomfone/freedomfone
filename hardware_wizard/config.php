63b5418d143eda2cc69083c6887b10f79d04c447
Alberto <aep@freedomfone.org>
2013-12-15 13:39:37 +0000

    Write auto-configuration to FS autoconf folder

--- hardware_wizard/config.php
+++ hardware_wizard/config.php
@@ -5,7 +5,7 @@ define("LogFile", "/opt/freedomfone/log/hardware_wizard.log");
 define("GammuConfig", "/opt/freedomfone/hardware_wizard/tmp/gammu.rc");
 define("HardwareDiscovery", "/opt/freedomfone/hardware_wizard/tmp/hardware-discovery");
 define("GammuConfigDir", "/opt/freedomfone/gammu-smsd/conf/");
-define("GsmOpenConfig", "/opt/freedomfone/hardware_wizard/tmp/gsmopen_config.xml");
+define("GsmOpenConfig", "/opt/freeswitch/conf/autoload_configs/gsmopen.conf.xml");
 
 $gammu_credentials = array(
 		     'user' => 'gammu',

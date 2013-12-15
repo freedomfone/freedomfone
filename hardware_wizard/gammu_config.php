ba9e0b9d6d3612789a9436c34541c462913b75bd
Alberto <aep@freedomfone.org>
2013-12-15 18:09:39 +0000

    integrated logs and debug level of gammu

--- hardware_wizard/gammu_config.php
+++ hardware_wizard/gammu_config.php
@@ -5,8 +5,10 @@ $data = file(HardwareDiscovery);
 foreach($data as $key => $entry){
 
  $entry = explode(",",$entry);
-print_r($entry); if($entry[6] == 'gammu'){
- $handle = fopen(GammuCondigDir."gammu-smsdrc-".$entry[0],'w');
+print_r($entry); 
+
+if($entry[6] == 'gammu'){
+ $handle = fopen(GammuConfigDir."gammu-smsdrc-".$entry[0],'w');
 
 
  fwrite($handle, "[gammu]\n");
@@ -15,13 +17,14 @@ print_r($entry); if($entry[6] == 'gammu'){
  fwrite($handle, "[smsd]\n");
  fwrite($handle, "service = sql\n");
  fwrite($handle, "driver = native_mysql\n");
- fwrite($handle, "Logfile = syslog\n");
+ fwrite($handle, "debuglevel = 3\n");
+ fwrite($handle, "Logfile = ".GammuLogDir."gammu-GM".($key+1)."-".$entry[0].".log\n");
  fwrite($handle, "user = ".$gammu_credentials['user']."\n");
  fwrite($handle, "password = ".$gammu_credentials['password']."\n");
  fwrite($handle, "pc = localhost\n");
  fwrite($handle, "MaxRetries = 5\n");
  fwrite($handle, "database = ".$gammu_credentials['database']."\n");
- fwrite($handle, "phoneid = ".$entry[0]."\n");
+ fwrite($handle, "phoneid = GM".($key+1)."-".$entry[0]."\n");
 
   fclose($handle);
  }

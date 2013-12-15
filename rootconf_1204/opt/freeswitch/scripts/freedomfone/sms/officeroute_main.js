0e6fec8c67ed78ca9904ffa73a2a9b7beba3e306
Alberto <aep@freedomfone.org>
2013-12-15 20:02:23 +0000

    Using login as a receiver tracker for SMS

--- rootconf_1204/opt/freeswitch/scripts/freedomfone/sms/officeroute_main.js
+++ rootconf_1204/opt/freeswitch/scripts/freedomfone/sms/officeroute_main.js
@@ -6,7 +6,7 @@ var date = argv[3];
 console_log(" [ " + sms + " ] " + sender +"\n");
     e = new Event("custom", "officeroute");
     e.addHeader("FF-SMS-Sender-Number",sender);
-    e.addHeader("FF-SMS-Receiver-Number",receiver);
+    e.addHeader("login",receiver);
     e.addHeader("proto","officeroute");
     e.addHeader("epoch",date);
     e.addBody(sms);

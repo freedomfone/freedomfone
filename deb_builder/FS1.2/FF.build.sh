eb26ecf0bf923f55e49682bfb94b68321b6237b1
Alberto <aep@freedomfone.org>
2013-12-17 13:31:02 +0000

    Deleting devel files

--- deb_builder/FS1.2/FF.build.sh
+++
@@ -1,5 +0,0 @@
-#autoreconf -fi (for yam)
-rm -Rf debian/libjs1-dbg/usr/lib/debug
-rm -Rf debian/libctb-dbg/usr/lib/debug
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/i386-linux-gnu/
-dpkg-buildpackage -b
--- fixes/20120119_startup.sh
+++
@@ -1,4 +0,0 @@
-cp /opt/freedomfone/fixes/20120119/rc.local /etc
-cp /opt/freedomfone/fixes/20120119/rc.local.freedomfone /etc
-cp /opt/freedomfone/fixes/20120119/freedomfone.conf /etc/init
-update-rc.d freeswitch disable

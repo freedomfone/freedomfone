508fd4bb99d02f15eb481926d4ae2aa416a70397
Louise <louise@freedomfone.org>
2014-05-09 14:03:38 +0000

    replaced del for delete

--- gui/app/Controller/MessagesController.php
+++ gui/app/Controller/MessagesController.php
@@ -290,7 +290,7 @@ class MessagesController extends AppController{
 
      	     $title = $this->Message->getTitle($id);
 
-    	     if($this->Message->del($id))
+    	     if($this->Message->delete($id))
 	     {
 	     $this->Session->setFlash('Message with title "'.$title.'" has been deleted.');
 	     $this->log('Msg: MESSAGE  DELETED; Id:title: '.$id.":".$title, 'leave_message');
@@ -329,7 +329,7 @@ class MessagesController extends AppController{
 	     	     if ($id) {
 			   $this->Message->id = $id;
 			   if ($action == __('Delete',true)){
-		     	       if ($this->Message->del($id)){
+		     	       if ($this->Message->delete($id)){
 				     $title = $this->Message->getTitle($id);
 				     $this->log('Msg: MESSAGE  DELETED; Id:title: '.$id.":".$title, 'leave_message');
 			        }

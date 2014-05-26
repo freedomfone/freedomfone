e683865a315f44f157085f8d2b406dbdd02d677d
Alberto <aep@freedomfone.org>
2014-05-26 07:57:30 +0000

    Sharicus Localization Update

--- l10n/migrate.sh
+++ l10n/migrate.sh
@@ -5,10 +5,10 @@
 #
 #################################################################
 
-FROM=manguensis
+FROM=/var/tmp/latest
 TO=sharicus
 #TO=suyo
-LANG="swa fre esp por"
+LANG="swa fra esp por"
 for i in $LANG; do
 echo "Migrating $i"
 pomigrate2 $FROM"/"$i"/LC_MESSAGES" $TO"/"$i"/LC_MESSAGES" "pot/"$TO
